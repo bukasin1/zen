6da4bdd9ba7e507f6ae4c1591eccc64526507164
Ebuka Okafor <bukasin1@gmail.com>
2026-04-14 16:27:39 +0100

    updated middleware registration

--- pkg/framework/app.go
+++ pkg/framework/app.go
@@ -5,15 +5,21 @@ import (
 )
 
 type App struct {
-	router      *Router
-	middlewares []Middleware
+	router            *Router
+	middlewares       []Middleware
+	systemMiddlewares []Middleware
 }
 
+// TODO: add new app configs (Probable future updates)
 func New() *App {
-	return &App{
+	app := &App{
 		router:      NewRouter(),
 		middlewares: []Middleware{},
+		// auto install system middlewares
+		systemMiddlewares: []Middleware{Recovery()},
 	}
+
+	return app
 }
 
 func (a *App) Use(m Middleware) {
@@ -21,11 +27,17 @@ func (a *App) Use(m Middleware) {
 }
 
 func (a *App) Get(path string, handler HandlerFunc) {
-	a.router.Handle(http.MethodGet, path, chainMiddlewares(handler, a.middlewares))
+	a.router.Handle(http.MethodGet, path, a.applyMiddlewares(handler))
 }
 
 func (a *App) Post(path string, handler HandlerFunc) {
-	a.router.Handle(http.MethodPost, path, chainMiddlewares(handler, a.middlewares))
+	a.router.Handle(http.MethodPost, path, a.applyMiddlewares(handler))
+}
+
+func (a *App) applyMiddlewares(h HandlerFunc) HandlerFunc {
+	h = chainMiddlewares(h, a.middlewares)
+	h = chainMiddlewares(h, a.systemMiddlewares)
+	return h
 }
 
 func (a *App) Listen(addr string) error {
