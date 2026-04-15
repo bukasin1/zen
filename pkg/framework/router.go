c6c9d03cdcad6f94aa7a46245aa5b5078b12275e
Ebuka Okafor <bukasin1@gmail.com>
2026-04-15 14:25:30 +0100

    refactor: update router to support nested method-path maps

--- pkg/framework/router.go
+++ pkg/framework/router.go
@@ -1,28 +1,44 @@
 package framework
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type HandlerFunc func(*Context)
 
 type Router struct {
-	routes map[string]HandlerFunc
+	routes map[string]map[string]HandlerFunc
 }
 
 func NewRouter() *Router {
 	return &Router{
-		routes: make(map[string]HandlerFunc),
+		routes: make(map[string]map[string]HandlerFunc),
 	}
 }
 
 func (r *Router) Handle(method, path string, handler HandlerFunc) {
-	key := method + ":" + path
-	r.routes[key] = handler
+	if _, ok := r.routes[method]; !ok {
+		r.routes[method] = make(map[string]HandlerFunc)
+	}
+	r.routes[method][path] = handler
+}
+
+func (r *Router) FindRoute(method, path string) (HandlerFunc, bool) {
+	methodRoutes, ok := r.routes[method]
+	if !ok {
+		return nil, false
+	}
+
+	pathHandler, ok := methodRoutes[path]
+	if !ok {
+		return nil, false
+	}
+
+	return pathHandler, true
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + ":" + req.URL.Path
-
-	if handler, ok := r.routes[key]; ok {
+	if handler, ok := r.FindRoute(req.Method, req.URL.Path); ok {
 		ctx := NewContext(w, req)
 		handler(ctx)
 		return
