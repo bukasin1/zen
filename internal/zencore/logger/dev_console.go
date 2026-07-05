d912cde3715768661ed599602c0014780c7dcf1c
Ebuka Okafor <bukasin1@gmail.com>
2026-07-05 19:00:11 +0100

    refactor: improve dev console logger formatting with sorted keys

--- internal/zencore/logger/dev_console.go
+++ internal/zencore/logger/dev_console.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"fmt"
+	"sort"
 	"time"
 
 	"github.com/bukasin1/zen/internal/zencore/internal/utils"
@@ -24,16 +25,33 @@ func (l *DevConsoleLogger) log(level string, color string, msg string, fields Fi
 		msg,
 	)
 
-	for k, v := range fields {
+	fieldsKeys := make([]string, 0, len(fields))
+	for k := range fields {
+		fieldsKeys = append(fieldsKeys, k)
+	}
+	sort.Strings(fieldsKeys)
+
+	for _, k := range fieldsKeys {
+		v := fields[k]
 		switch k {
 		case "addr":
 			fmt.Printf(" on %shttp://localhost%v%s", utils.ColorYellow, v, utils.ColorReset)
 			continue
 		case "status":
 			statusCol := utils.StatusColor(v.(int))
-			v = fmt.Sprintf("%s%d%s", statusCol, v, utils.ColorReset)
+			v = fmt.Sprintf("%s%-3d%s", statusCol, v, utils.ColorReset)
 		case "method":
-			v = fmt.Sprintf("%s%s%s", utils.ColorBlue, v, utils.ColorReset)
+			v = fmt.Sprintf("%s%-6s%s", utils.ColorBlue, v, utils.ColorReset)
+		case "duration":
+			v = fmt.Sprintf("%s%-6s%s", utils.ColorGreen, v, utils.ColorReset)
+		case "path":
+			v = fmt.Sprintf("%s%s%s", utils.ColorYellow, v, utils.ColorReset)
+		case "size":
+			v = fmt.Sprintf("%s%d%s", utils.ColorGray, v, utils.ColorReset)
+		// case "requestID":
+		// 	v = fmt.Sprintf("%s%-16s%s", utils.ColorGray, v, utils.ColorReset)
+		// case "ip":
+		// 	v = fmt.Sprintf("%s%-16s%s", utils.ColorGray, v, utils.ColorReset)
 		default:
 		}
 
