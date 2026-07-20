89782533881f01a32fc8e9ce8622384f86453a5f
Ebuka Okafor <bukasin1@gmail.com>
2026-07-20 13:52:56 +0100

    chore: remove piscine-checkpoint algorithms and cleanup project structure

--- Algorithms/piscine-checkpoint/concatslice.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func ConcatSlice(slice1, slice2 []int) []int {
-	return append(slice1, slice2...)
-}
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/bukasin1/zen
 
 go 1.26.1
-
-require github.com/01-edu/z01 v0.1.0
